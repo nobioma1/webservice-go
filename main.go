bfbeb5253e6864d2001f365a6c95962f7b8021d6
Noble Obioma <nobioma1@gmail.com>
2020-04-03 16:46:04 +0100

    Possible ways of declaring variables

--- main.go
+++ main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, Web Service")
+	// variable declarations
+
+	// Verbose declaration
+	var i int // decalaration and type
+	i = 10    // setting to an integer
+	fmt.Println(i)
+
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	// implicit initialization syntax
+	isTrue := false
+	fmt.Println(isTrue)
 }
