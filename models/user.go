027e1375f3aaa7478c4b92b21916854a3e3d21e4
Noble Obioma <nobioma1@gmail.com>
2020-04-06 09:34:30 +0100

    Add models to GetUserByID, UpdateUserByID, RemoveUserById. Return error mesage if user ID is passed on AddUser

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 // User struct
 type User struct {
 	ID        int
@@ -20,9 +25,46 @@ func GetUsers() []*User {
 
 // AddUser add a new user to users slice passing the user object
 func AddUser(user User) (User, error) {
+	if user.ID != 0 {
+		return User{}, errors.New("User must not include ID")
+	}
+
 	user.ID = nextID
 	nextID++
 	users = append(users, &user)
 
 	return user, nil
+}
+
+// GetUserByID return the user struct with id
+func GetUserByID(id int) (User, error) {
+	for _, user := range users {
+		if user.ID == id {
+			return *user, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with '%v' not found", id)
+}
+
+// UpdateUserByID updates a user with matching id
+func UpdateUserByID(update User) (User, error) {
+	for index, user := range users {
+		if user.ID == update.ID {
+			users[index] = &update
+			return update, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with ID '%v' not found", update.ID)
+}
+
+// RemoveUserByID removes user with ID
+func RemoveUserByID(id int) (int, error) {
+	for index, user := range users {
+		if user.ID == id {
+			users = append(users[:index], users[index+1:]...)
+			return id, nil
+		}
+	}
+	return -1, fmt.Errorf("User with '%v' not found", id)
 }
